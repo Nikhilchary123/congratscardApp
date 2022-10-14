3591ceadfa5ca3317b31347ef627bbb4df4426b0
Nikhilchary123 <avusulanikhil123@gmail.com>
2022-10-14 06:55:41 +0000

    congratsCard

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
+//
 const element = (
-    <div className="bgcontainer">
-        <h1 className="heading">Congratulations</h1>
-        <div className=innerbgcontainer>
-            <img className="profile-image" src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"/>
-            <h1 className="name">Kiran.V</h1>
-            <p class="para">Vishnu Institute of Computer Education and Technology, Bhimavaram</p>
-            <img/ className="watch-image" src"https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"/>
-
-        </div>
-
+  <div className="congrats-card-container">
+    <h1 className="heading">Congratulations</h1>
+    <div className="card">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"
+        className="profile-image"
+      />
+      <h1 className="card-title">Kiran V</h1>
+      <p className="card-description">
+        Vishnu Institute of Computer Education and Technology, Bhimavaram
+      </p>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
+        className="watch-image"
+      />
     </div>
-  )
-  
-ReactDOM.render(element, document.getElementById('root'))
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
